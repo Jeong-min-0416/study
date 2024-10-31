ffe002ecc28d19c863e7550739a77f158829d179
이정민 <ijeong0416@gmail.com>
2024-10-31 17:12:29 +0900

    기본 환경 세팅

---
+++ src/main/java/com/example/study/domain/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.study.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+}
